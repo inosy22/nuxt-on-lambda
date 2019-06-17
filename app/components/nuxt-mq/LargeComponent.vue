b53f0893c4da62e714c89c6f9c76217a5a69be97
inosy22 <inos.sj@gmail.com>
2019-06-17 16:09:53 +0900

    add example nuxt-mq

---
+++ app/components/nuxt-mq/LargeComponent.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="example">I am Large!</div>
+</template>
+
+<style scoped src="./nuxt-mq-component.css"></style>
+<style scoped>
+.example {
+  background-color: yellow;
+}
+</style>
